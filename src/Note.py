01b59a2bb5bd65ca9282b6013be5b63a65a5b164
ross <rossbm94@gmail.com>
2020-03-08 19:34:54 -0400

    A little added for saving but very much WIP
    Signed-off-by: Ross M <rossbm94@gmail.com>

--- src/Note.py
+++ src/Note.py
@@ -1,13 +1,18 @@
 import time
+import pickle
 
 
 class Note:
 
-    def __init__(self, initial_content: str = ""):
-        self.TimesStamps = list()
-        self.TimesStamps.append(time.time())
+    def __init__(self, title: str, initial_content: str = ""):
+        self.Title = title
+        self.TimesStamps = dict()
+        self.TimesStamps[time.time()] = None
         self.content = initial_content
         self.Nodes = dict()
 
-    def save(self, name):
-        pass
+    def save(self, name: str = ""):
+        if len(name) is 0:
+            name = self.Title
+        file = open(name, 'wb')
+        pickle.dump(self, file)
--- test/test_Note.py
+++ test/test_Note.py
@@ -5,18 +5,20 @@ from src import Note
 class TestNote(unittest.TestCase):
 
     def setUp(self):
-        self.note = Note.Note()
+        self.note = Note.Note("Test")
 
     def test_init(self):
         # assert
-        self.assertEquals(1, len(self.note.TimesStamps))
-        self.assertEquals("", self.note.content)
+        self.assertEqual("Test", self.note.Title)
+        self.assertEqual("", self.note.content)
+        self.assertEqual(1, len(self.note.TimesStamps))
+        first = [v for v in self.note.TimesStamps.values()]
+        self.assertIsNone(first[0])
 
     def test_save(self):
         # arrange
-        file_name = "note1"
         # act
-        self.note.save(file_name)
+        self.note.save()
         # assert
         self.assertFalse(True)
 
