8f94c76d739337126c83d6315bda0ad1d05732a0
Ross Mattingly <rbmattingly@zohomail.com>
2020-03-17 20:22:04 -0400

    Not in best state. Need a way to not add the contents of TimeStamp to my comparison, which should be fairly simple.

--- src/Note.py
+++ src/Note.py
@@ -10,36 +10,60 @@ class Note:
         self.TimeStamps = dict()
         self.__Content = initial_content
         self.__Nodes = dict()
-        diff = difflib.unified_diff("", yaml.dump(self).splitlines(1))
-        diff = list(diff)
-        self.TimeStamps[time.time()] = ''.join(diff) 
+        self._update_time_stamps()
 
     @property
-    def Title(self):
+    def title(self):
         return self.__Title
 
-    @Title.setter
-    def Title(self, Title):
-        self.__Title = Title
-    
+    @title.setter
+    def title(self, title):
+        self.__Title = title
+
     @property
-    def Content(self):
+    def content(self):
         return self.__Content
 
-    @Content.setter
-    def Content(self, Content):
-        self.__Content = Content
+    @content.setter
+    def content(self, content):
+        self.__Content = content
+        self._update_time_stamps()
 
     @property
-    def Nodes(self):
+    def nodes(self):
         return self.__Nodes
 
-    @Nodes.setter
-    def Nodes(self, Nodes):
-        self.__Nodes = Nodes
+    @nodes.setter
+    def nodes(self, nodes):
+        self.__Nodes = nodes
 
+    def _update_time_stamps(self):  # , previous):
+        # diff = difflib.ndiff(previous.splitlines(), yaml.dump(self).splitlines(1))
+        # diff = ''.join(diff)
+        previous_lines = list()
+        if len(self.TimeStamps) > 0:
+            previous_lines = self.TimeStamps[max(self.TimeStamps.keys())].split('\\n')
 
-	
+        previous_length = len(previous_lines)
+        for i in range(previous_length):
+            previous_lines[i] = previous_lines[i][3:]
+        current_lines = yaml.dump(self).split('\n')
+        current_length = len(current_lines)
+        result = list()
+        for i in range(current_length):
+            if i >= len(previous_lines):
+                result.append(str(i)+"+ "+current_lines[i])
+                continue
+            if previous_lines[i] == current_lines[i]:
+                continue
+            result.append(str(i)+"c "+previous_lines[i]+"-->>"+current_lines[i])
+        if current_length < previous_length:
+            for i in range(previous_length - current_length):
+                index = current_length+i
+                result.append(str(index)+"- "+previous_lines[index])
+
+        self.TimeStamps[time.time()] = '\\n'.join(result)
+
     def save(self, name: str = ""):
         if len(name) == 0:
-            name = self.Title
+            name = self.title
--- test/test_Note.py
+++ test/test_Note.py
@@ -9,11 +9,16 @@ class TestNote(unittest.TestCase):
 
     def test_init(self):
         # assert
-        self.assertEqual("Test", self.note.Title)
-        self.assertEqual("", self.note.Content)
+        self.assertEqual("Test", self.note.title)
+        self.assertEqual("", self.note.content)
         self.assertEqual(1, len(self.note.TimeStamps))
         first = [v for v in self.note.TimeStamps.values()]
-        self.assertEqual("--- \n+++ \n@@ -0,0 +1,5 @@\n+!!python/object:src.Note.Note\n+TimeStamps: {}\n+_Note__Content: ''\n+_Note__Nodes: {}\n+_Note__Title: Test\n", first[0], 'first diff was:\n'+first[0])
+        first = first[0].split('\\n')
+        self.assertEqual("0+ !!python/object:src.Note.Note", first[0])
+        self.assertEqual("1+ TimeStamps: {}", first[1])
+        self.assertEqual("2+ _Note__Content: ''", first[2])
+        self.assertEqual("3+ _Note__Nodes: {}", first[3])
+        self.assertEqual("4+ _Note__Title: Test", first[4])
 
     def test_save(self):
         # arrange
@@ -21,17 +26,16 @@ class TestNote(unittest.TestCase):
         self.note.save()
         # assert
         self.assertFalse(True)
-        
+
     def test_TimeStampsContent(self):
-      # arrange
-      self.note.Content = "new stuff"
-      # act
-      result = self.note.TimeStamps
-      # assert
-      self.assertEqual(2, len(result))
-      self.assertEqual("+_Note__Content: 'new stuff'\n", result[max(result.keys())])
-
-
+        # arrange
+        self.note.content = "new stuff"
+        # act
+        result = self.note.TimeStamps
+        # assert
+        self.assertEqual(2, len(result))
+        diff = result[max(result.keys())].split('\\n')
+        self.assertEqual("9c _Note__Content: ''-->>_Note__Content: new stuff", diff[0])
 
 
 if __name__ == '__main__':
